edf5383f750ffeb9b1c1d977dc2ea93f28556c41
Rohit Kumar Pandit <77102068+rohit-64bit@users.noreply.github.com>
2023-04-03 11:42:27 +0530

    latest and secured deploy for production

--- routes/adminAuth.js
+++ routes/adminAuth.js
@@ -13,44 +13,6 @@ const env = process.env;
 
 jwtSecret = env.JWT_SECRET_ADMIN;
 
-// ROUTE 1: create a Admin using : POST '/api/auth/Admin/createAdmin' Doesn't require auth
-
-router.post('/createadmin', [
-    body('email', 'Enter a valid e-mail').isEmail(),
-    body('password', 'Password must be atleast 8 characters').isLength({ min: 8 }),
-], async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    try {
-        let admin = await Admin.findOne({ email: req.body.email });
-        if (admin) {
-            return res.status(400).json({ error: "Sorry a Admin with this email already exists" });
-        }
-        const salt = await bcrypt.genSalt(10)
-        const secPass = await bcrypt.hash(req.body.password, salt);
-
-        // create a new admin
-        admin = await Admin.create({
-            email: req.body.email,
-            password: secPass
-        })
-        const data = {
-            admin: {
-                id: admin.id
-            }
-        }
-        const authtoken = jwt.sign(data, jwtSecret)
-
-        // res.json(Admin)
-        res.json({ authtoken })
-    } catch (errors) {
-        console.error(errors.message);
-        res.status(500).send("Internal Server Error");
-    }
-})
-
 // ROUTE 2 : Authenticate a Admin using : POST '/api/auth/admin/authAdmin' Require auth
 
 router.post('/authadmin', [
